cd05a946a97dc3c457d54eea4f928121b44433ff
hikaruk <hikaruk@C010423221.zipangu.cac.co.jp>
2026-04-16 15:16:07 +0900

    Question07

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_1.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_1.java
@@ -1,12 +1,58 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_1 {
 
 	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-		// TODO:ここに実装
+		String[] itemName = { "a", "b", "c", "d", "e" };
+		int[] Price = { 0, 0, 0, 0, 0 };
+
+		System.out.println("商品1を入力してください：＞");
+		itemName[0] = reader.readLine();
+
+		System.out.println("商品2を入力してください：＞");
+		itemName[1] = reader.readLine();
+
+		System.out.println("商品3を入力してください：＞");
+		itemName[2] = reader.readLine();
+
+		System.out.println("商品4を入力してください：＞");
+		itemName[3] = reader.readLine();
+
+		System.out.println("商品5を入力してください：＞");
+		itemName[4] = reader.readLine();
+
+		System.out.println("\n各商品の単価を順に入力してください。\n");
+
+		System.out.println(itemName[0] + "の単価：＞");
+		String itemPrice1 = reader.readLine();
+		Price[0] = Integer.parseInt(itemPrice1);
+
+		System.out.println(itemName[1] + "の単価：＞");
+		String itemPrice2 = reader.readLine();
+		Price[1] = Integer.parseInt(itemPrice2);
+
+		System.out.println(itemName[2] + "の単価：＞");
+		String itemPrice3 = reader.readLine();
+		Price[2] = Integer.parseInt(itemPrice3);
+
+		System.out.println(itemName[3] + "の単価：＞");
+		String itemPrice4 = reader.readLine();
+		Price[3] = Integer.parseInt(itemPrice4);
+
+		System.out.println(itemName[4] + "の単価：＞");
+		String itemPrice5 = reader.readLine();
+		Price[4] = Integer.parseInt(itemPrice5);
+
+		System.out.println("登録商品一覧");
+		for (int i = 0; i < 5; i++) {
+			System.out.println(itemName[i] + "：" + Price[i] + "円");
+		}
 
 	}
 
